fa36a4d78c8ecea6e61d6da5737e71ac87dd5e73
zhengweifeng <zhengwf@kamfu.com.cn>
2018-03-20 20:00:17 +0800

    日常修改

--- Arithmetic.h
+++ Arithmetic.h
@@ -2,15 +2,6 @@
 #include<opencv2\opencv.hpp>
 using namespace cv;
 
-
-/*************************************
-//									//
-//									//
-//			图像算法					//
-//									//
-//									//
-*************************************/
-
 class Arithmetic
 {
 public:
@@ -21,7 +12,6 @@ public:
 	void Arithmetic_Canny();	//边缘
 	void Arithmetic_HoughLines();	//标准霍夫线变换
 	void Arithmetic_HoughLinesP();	//统计概率霍夫线变换
-	//void Arithmetic_EqualizeHist();	// 应用直方图均衡化
 
 	~Arithmetic();
 };
--- Blur.h
+++ Blur.h
@@ -2,40 +2,20 @@
 #include<opencv2\opencv.hpp>
 using namespace cv;
 
-
-/*************************************
-//									//
-//			图像平滑处理				//
-//									//
-//			图像降噪处理				//
-//									//
-*************************************/
-
-/*
-图像降噪处理
-同时显示4种降噪
-单独使用 ：
-		均值平滑 高斯平滑 中值平滑 双边平滑
-*/
 class Blur
 {
 public:
 	Blur(Mat src);
 	~Blur();
-
-	/*全部显示处理*/
+	//全部显示处理
 	void View();
-
-	/*均值平滑*/
+	//均值
 	void Blur_junzhi();
-
-	/*高斯平滑*/
+	//高斯
 	void GaussianBlur();
-
-	/*中值平滑*/
+	//中值
 	void MedianBlur();	
-
-	/*双边平滑*/
+	//双边
 	void BilateralFilter();
 };
 
