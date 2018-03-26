e73b0c8a2254327028e76edae9b896d89f879945
zhengweifeng <zhengwf@kamfu.com.cn>
2018-03-26 18:35:34 +0800

    新建特征类

---
+++ Feature.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include "Feature.h"
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/core/core.hpp"
+
+Feature::Feature()
+{
+}
+
+void Feature::FindFeature() {
+	DescriptorExtractor a;
+}
+
+Feature::~Feature()
+{
+}
---
+++ Feature.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/core/core.hpp"
+
+using namespace std;
+using namespace cv;
+
+class Feature
+{
+public:
+	Feature();
+
+	//ÌØÕ÷µã¼ì²â
+	void FindFeature();
+
+
+	~Feature();
+};
+
