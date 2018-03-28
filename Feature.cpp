77d80a5570c00b19881508d36ccce191fbfb770f
yw0lk1nni9ht <641195340@qq.com>
2018-03-28 23:49:51 +0800

    日常更新

--- Feature.cpp
+++ Feature.cpp
@@ -8,16 +8,18 @@ Feature::Feature()
 }
 
 void Feature::FindFeature() {
-	Mat src22 = imread("piaohao.jpg", CV_LOAD_IMAGE_GRAYSCALE);
-	vector<KeyPoint> keypoints;
-	//int minHessian = 400;
-	//DescriptorExtractor a;
-	AGAST(src22,keypoints,10);
-	//FAST(src22, keypoints, 100);
-	//AgastFeatureDetector c;
-	//c.detect(src22, keypoints);
+	Mat src22 = imread("µ¥×Ö\\µÚ12ÕÅ£º0.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+	namedWindow("src", 0);
+	imshow("src", src22);
 	Mat out_src;
+
+	vector<KeyPoint> keypoints;
+	AGAST(src22,keypoints,50);
+	//FAST(src22, keypoints, 100);
+
+	
 	drawKeypoints(src22, keypoints, out_src);
+	namedWindow("look", 0);
 	imshow("look", out_src);
 }
 
