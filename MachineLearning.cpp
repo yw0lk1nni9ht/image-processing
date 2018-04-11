be32faea9e95e584b9c5f21e0d5051776993bd46
zhengweifeng <zhengwf@kamfu.com.cn>
2018-04-11 17:56:35 +0800

    svm分类，分类全同类

--- MachineLearning.cpp
+++ MachineLearning.cpp
@@ -59,46 +59,101 @@ void MachineLearning::KNN_test()
 void MachineLearning::SVM_test() {
 
 	Mat train_data, train_label;
-	Mat targetData, backData;	//正样本/负样本
+	Mat targetData,backData;	//正样本/负样本
 
 	Mat muban = imread("模板\\0\\0.jpg", 0);
+	int _rows = muban.rows;	//标准行
+	int _cols = muban.cols;	//标准列
+	int plus_num = 30;	//正样本数量
+	int minus_num = 36;	//负样本数量
+	/*Mat targetData(plus_num, _rows*_cols, CV_32FC1);
+	Mat backData(minus_num, _rows*_cols, CV_32FC1);*/
+
 	//正样本
-	for (int i = 0; i < 5; i++) {
-		Mat tmp = imread("模板\\0\\" + to_string(i) + ".jpg", CV_32FC1);
-		normalize(muban, tmp);
-		tmp.reshape(0, 1);
-		Mat _tmp = targetData.rowRange(i, i + 1);
-		tmp.copyTo(_tmp);
+	for (int i = 0; i < plus_num; i++) {
+		Mat tmp = imread("模板\\0\\" + to_string(i) + ".jpg", 0);
 		
+		tmp.rows = _rows;
+		tmp.cols = _cols;
+		
+		//tmp.reshape(1, 1);
+		/*Mat _tmp = targetData.rowRange(i, i + 1);
+		_tmp = tmp.reshape(1, 1);*/
+		//tmp.copyTo(_tmp);
+		Mat _tmp;
+		tmp.convertTo(_tmp, CV_32FC1);
+		_tmp = _tmp.reshape(1, 1);
 		targetData.push_back(_tmp);
+		train_label.push_back(0);
 	}
 
 	//负样本
 	for (int i = 1; i < 10; i++) {
-		Mat tmp = imread("模板\\" + to_string(i) + "\\0.jpg", CV_32FC1);
-		normalize(muban, tmp);
-		backData.push_back(tmp);
+		for (int j = 0; j < 4; j++) {
+			Mat tmp = imread("模板\\" + to_string(i) + "\\" + to_string(j) +".jpg", 0);
+			tmp.rows = _rows;
+			tmp.cols = _cols;
+			/*tmp.reshape(1, 1);
+			Mat _tmp = backData.rowRange(i - 1, i);
+			tmp.copyTo(_tmp);*/
+
+			Mat _tmp;
+			tmp.convertTo(_tmp, CV_32FC1); 
+			_tmp = _tmp.reshape(1, 1);
+			backData.push_back(_tmp);
+			train_label.push_back(i);
+		}
 	}
 	vconcat(targetData, backData, train_data); //合并所有的样本点，作为训练数据
-	train_label = Mat(14, 1, CV_32S, Scalar::all(1)); //初始化标注
-	for (int i = 5; i < 14; i++) {
-		train_label.at<int>(i, 0) = -1;
-	}
+	//train_label = Mat(plus_num + minus_num, 1, CV_32S, Scalar::all(1)); //初始化标注
+	//train_label = Mat(plus_num + minus_num, 1, CV_32F, Scalar::all(1)); //初始化标注
+	/*for (int i = plus_num; i < plus_num + minus_num; i++) {
+		train_label.at<int>(i, 0) = 0;
+	}*/
 
 
 	Ptr<SVM> svm = SVM::create();
-	svm->setType(SVM::C_SVC);
+	svm->setType(100);	//100-104
 	svm->setKernel(SVM::LINEAR);
+	svm->setTermCriteria(TermCriteria(TermCriteria::MAX_ITER, 100, 1e-6));
 	
 	// 训练分类器
-	Ptr<TrainData> traindata = ml::TrainData::create(train_data, ROW_SAMPLE, train_label);
+	Ptr<TrainData> traindata = TrainData::create(train_data, ROW_SAMPLE, train_label);
+	//svm->train(traindata);
 	svm->trainAuto(traindata, 10);
 	svm->save("svm.xml");
 
-	Mat sampleMat = imread("单字\\第11张：0.jpg", CV_32FC1);
-	float response = svm->predict(sampleMat);  //进行预测，返回1或-1,返回类型为float
-	Mat sampleMat2 = imread("单字\\第11张：5.jpg", CV_32FC1);
-	float response2 = svm->predict(sampleMat);  //进行预测，返回1或-1,返回类型为float
+	
+	Mat sampleMat = imread("单字\\第11张：0.jpg", 0);
+	sampleMat.rows = _rows;
+	sampleMat.cols = _cols;
+	Mat test;
+	sampleMat.convertTo(test, CV_32FC1);
+	test = test.reshape(1, 1);
+	float response = svm->predict(test);  //进行预测，返回1或-1,返回类型为float
+
+	Mat sampleMat2 = imread("单字\\第17张：5.jpg", CV_32FC1);
+	sampleMat2.rows = _rows;
+	sampleMat2.cols = _cols;
+	sampleMat.convertTo(test, CV_32FC1);
+	test = test.reshape(1, 1);
+	float response2 = svm->predict(test);  //进行预测，返回1或-1,返回类型为float
+
+	Mat sampleMat3 = imread("单字\\第12张：5.jpg", CV_32FC1);
+	sampleMat3.rows = _rows;
+	sampleMat3.cols = _cols;
+	sampleMat3 = sampleMat.reshape(1, 1);
+	test = Mat(sampleMat3.rows, sampleMat3.cols, CV_32F);
+	sampleMat3.convertTo(test, CV_32F);
+	float response3 = svm->predict(test);  //进行预测，返回1或-1,返回类型为float
+
+	Mat sampleMat4 = imread("单字\\第13张：5.jpg", CV_32FC1);
+	sampleMat4.rows = _rows;
+	sampleMat4.cols = _cols;
+	sampleMat4 = sampleMat.reshape(1, 1);
+	test = Mat(sampleMat4.rows, sampleMat2.cols, CV_32F);
+	sampleMat4.convertTo(test, CV_32F);
+	float response4 = svm->predict(test);  //进行预测，返回1或-1,返回类型为float
 }
 
 
